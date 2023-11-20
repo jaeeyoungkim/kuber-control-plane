8d4442cf0970798317c68c532c3443cb9e32c5db
jykim <jykim@whatap.io>
2023-11-20 21:26:18 +0900

    added getContainerId

--- getContainerId.go
+++ getContainerId.go
@@ -3,81 +3,25 @@ package main
 import (
 	"context"
 	"fmt"
-	"strings"
+	"log"
 
-	io_prometheus_client "github.com/prometheus/client_model/go"
-	"github.com/prometheus/common/expfmt"
-	"k8s.io/client-go/kubernetes"
-	k8srest "k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
+	"github.com/containerd/containerd"
 )
 
-type apiServerRequestTotal struct {
-	code        string
-	verb        string
-	Driver      string
-	Mount       string
-	Total       int64
-	Used        int64
-	UsedPercent float64
-}
-
-func getApiserverRequestTotal(metric *io_prometheus_client.MetricFamily) {
-	for _, m := range metric.Metric {
-		fmt.Printf("Labels:\n")
-		for _, l := range m.Label {
-			fmt.Printf("%s: %s\n", l.GetName(), l.GetValue())
-		}
-		fmt.Printf("Value: %v\n", m.GetCounter().GetValue())
-		fmt.Printf("\n")
-	}
-}
-
 func main() {
-	config, err := k8srest.InClusterConfig()
+	client, err := containerd.New("/run/containerd/containerd.sock")
 	if err != nil {
-		fmt.Printf("error getting in-cluster config: %v\n", err)
-		kubeconfig := "/Users/jaeyoung/.kube/config"
-		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
-		if err != nil {
-			fmt.Printf("error getting out-of-cluster config: %v\n, err")
-			return
-		}
+		log.Fatal(err)
 	}
-	clientset, err := kubernetes.NewForConfig(config)
+	defer client.Close()
+
+	ctx := context.Background()
+	containers, err := client.Containers(ctx)
 	if err != nil {
-		fmt.Printf("error creating clientset: %v\n", err)
-		return
+		log.Fatal(err)
 	}
 
-	bytes, err := clientset.RESTClient().Get().AbsPath("/metrics").DoRaw(context.Background())
-	if err != nil {
-		fmt.Printf("error getting metrics: %v\n", err)
-		return
+	for _, container := range containers {
+		fmt.Println(container.ID())
 	}
-
-	var parser expfmt.TextParser
-	metricFamilies, err := parser.TextToMetricFamilies(strings.NewReader(string(bytes)))
-	if err != nil {
-		fmt.Printf("error parsing metrics: %v\n", err)
-		return
-	}
-
-	for key := range metricFamilies {
-		fmt.Println(key)
-	}
-
-	var apiserverRequestTotalMetrics *io_prometheus_client.MetricFamily
-	apiserverRequestTotalMetrics = metricFamilies["apiserver_request_total"]
-	_ = apiserverRequestTotalMetrics
-	// getApiserverRequestTotal(apiserverRequestTotalMetrics)
-	// for _, m := range metric.Metric {
-	// 	fmt.Printf("Labels:\n")
-	// 	for _, l := range m.Label {
-	// 		fmt.Printf("%s: %s\n", l.GetName(), l.GetValue())
-
-	// 	}
-	// 	fmt.Printf("Value: %v\n", m.GetCounter().GetValue())
-	// 	fmt.Printf("\n")
-	// }
 }
