921d4c157912f3a18f110dd7e74b8ad2fb137cd0
jykim <jykim@whatap.io>
2023-12-20 11:34:25 +0900

    test

---
+++ cmd/jykim/test-main/main.go
@@ -0,0 +1,18 @@
+package jykim
+
+import "fmt"
+
+func main() {
+	//metrics := GetApiserverRequestTotalMetrics()
+	//c := CommonResponse[[]ApiServerRequestTotalBucket]{
+	//	Data:    metrics,
+	//	Message: "this is ok",
+	//}
+	//marshal, err := json.Marshal(c)
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//fmt.Println(string(marshal))
+	fmt.Println("hello world")
+}
